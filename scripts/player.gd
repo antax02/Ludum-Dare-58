107b8b0e86ede4c996f01022c9dff5b28ddbaa8c
Jiwwi <lazyjiwwi@gmail.com>
2025-10-04 20:39:58 +0200

    Refactor player controls to use thrust input
    Replaces directional movement and dash inputs with a single 'thrust' action mapped to the spacebar. Updates player.gd to use 'thrust' for movement towards the mouse and adjusts braking power for smoother deceleration.

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,24 +19,13 @@ func shoot():
 	get_parent().add_child(b)
 
 func _physics_process(delta):
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("dash"):
+	if Input.is_action_pressed("thrust"):
 		velocity += (get_global_mouse_position() - global_position).normalized() * dash_power
 	if Input.is_action_pressed("break"):
-		velocity = velocity.normalized() * max(velocity.length() - 150 * delta, 0)
+		velocity = velocity.normalized() * max(velocity.length() - 200 * delta, 0)
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 
 	get_global_mouse_position()
 	global_rotation = lerp_angle(global_rotation, (get_global_mouse_position() - global_position).angle(), rotation_speed * delta)
-
 	move_and_slide()
-	#if velocity.length() > 0: 
-		#velocity = velocity.normalized() * speed
