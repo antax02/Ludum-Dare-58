b73949339d47b3bb5238012373789c6465ff4c8e
Jiwwi <lazyjiwwi@gmail.com>
2025-10-04 19:08:10 +0200

    Add bullet shooting functionality to player
    Introduces a bullet scene and script, updates player scene and script to support shooting bullets. Adds input mapping for shooting, a shoot marker for spawn position, and logic to instantiate and fire bullets. Bullets interact with enemies and are removed on collision.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,13 +1,19 @@
 extends CharacterBody2D
 
+@export var Bullet : PackedScene
 var health = 100
-var dmg = 10
-var bullet_speed
 var rotation_speed = 2
 var dash_power = 5
 
 func _ready():
 	pass
+
+func shoot():
+	var b = Bullet.instantiate()
+	b.global_position = $Shoot.global_position
+	b.global_rotation = $Shoot.global_rotation
+	get_parent().add_child(b)
+	b.transform = $Shoot.transform
 
 func _physics_process(delta):
 	if Input.is_action_pressed("move_up"):
@@ -21,7 +27,9 @@ func _physics_process(delta):
 	if Input.is_action_pressed("dash"):
 		velocity += (get_global_mouse_position() - global_position).normalized() * dash_power
 	if Input.is_action_pressed("break"):
-		velocity = velocity.normalized() * max(velocity.length() - 50 * delta, 0)
+		velocity = velocity.normalized() * max(velocity.length() - 150 * delta, 0)
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 	get_global_mouse_position()
 	global_rotation = lerp_angle(global_rotation, (get_global_mouse_position() - global_position).angle(), rotation_speed * delta)
