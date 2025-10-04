a183e18d8fb8a22961c4337e0f94efbb0f60304b
InThing <einteisak@gmail.com>
2025-10-04 18:54:18 +0200

    Add fast enemy type and improve enemy behavior
    Introduced a FastEnemy class with reduced health and created its scene. Refactored Enemy to use class_name, added target following logic, and updated world scene to use new enemy path. Also moved enemy assets to 'enemies' directory for better organization.

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,9 +1,25 @@
-extends CharacterBody2D
+class_name Enemy extends CharacterBody2D
 
 var target: Node2D
 var health: int = 100
 var damage: int = 10
-var speed: int = 4
+var speed: int = 100
+var rotation_speed: int = 5
+var distance: int = 200
 
-func player_target():
+
+func setup():
+	pass
+
+func _ready():
+	setup()
+
+func _physics_process(delta):
+	follow_target(delta)
+
+func follow_target(delta):
 	target = get_tree().get_first_node_in_group("player")
+	rotation = lerp_angle(rotation, (target.global_position - global_position).angle(), -(cos(PI * rotation_speed * delta) - 1) / 2)
+	
+	if position.distance_to(target.position) > distance:
+		position = position.move_toward(target.position, speed * delta)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,1 @@
 extends CharacterBody2D
-
-func _ready() -> void:
-	velocity.x = 30
-
-func _physics_process(delta: float) -> void:
-	
-	move_and_slide()
