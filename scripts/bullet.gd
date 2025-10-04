9b98c71ee9b2c53ced7afc5dc4601b7245aa57b7
Jiwwi <lazyjiwwi@gmail.com>
2025-10-04 19:17:14 +0200

    Add enemy group and bullet collision handling
    Enemies are now assigned to the 'enemies' group, and bullets detect collision with enemies using the 'body_entered' signal. When a bullet collides with an enemy, both are removed from the scene.

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -5,7 +5,7 @@ var speed = 750
 func _physics_process(delta):
 	position += transform.x * speed * delta
 
-func _on_Bullet_body_entered(body):
+func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemies"):
 		body.queue_free()
 	queue_free()
