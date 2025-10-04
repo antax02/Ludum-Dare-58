6299a083c144b800efd9bdac625ac2a3187584f9
Jiwwi <lazyjiwwi@gmail.com>
2025-10-04 18:05:36 +0200

    Add player movement and input actions
    Defined custom input actions for movement, dash, and break in project settings. Updated player.gd to handle movement, dashing towards mouse, and braking. Added player variables for health, damage, bullet speed, and rotation speed. Minor update to asteroid scene metadata.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,31 @@
 extends CharacterBody2D
 
-func _ready() -> void:
-	velocity.x = 200
+var health = 100
+var dmg = 10
+var bullet_speed
+var rotation_speed = 2
+var dash_power = 5
 
-func _physics_process(delta: float) -> void:
+func _ready():
+	pass
+
+func _physics_process(delta):
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("dash"):
+		velocity += (get_global_mouse_position() - global_position).normalized() * dash_power
+	if Input.is_action_pressed("break"):
+		velocity = velocity.normalized() * max(velocity.length() - 50 * delta, 0)
+
+	get_global_mouse_position()
+	global_rotation = lerp_angle(global_rotation, (get_global_mouse_position() - global_position).angle(), rotation_speed * delta)
+
 	move_and_slide()
+	#if velocity.length() > 0: 
+		#velocity = velocity.normalized() * speed
