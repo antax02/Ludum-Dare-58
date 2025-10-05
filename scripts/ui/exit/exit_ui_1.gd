de49024bd51098441624269a4b0c1884d92ec3f1
InThing <einteisak@gmail.com>
2025-10-05 17:13:41 +0200

    Refactor exit zone scripts and remove unused UI
    Moved exit_zone.gd and shop_exit_zone.gd scripts to scripts/environment/exits for better organization. Updated scene references to use new script paths. Removed unused exit UI script and node from level_1.tscn.

--- scripts/ui/exit/exit_ui_1.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-const LEVEL_2 = preload("res://scenes/levels/level_2.tscn")
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/level_2.tscn")
