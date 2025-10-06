482803933824294b08ac3a8466297c57cb7d07a5
antax02 <anton.axelsson@nyckelpiga.net>
2025-10-06 22:50:16 +0200

    Update main menu layout and audio handling
    Refactored main menu scene structure, moving ClickAudio under menu/marign and updating its parent path. Added click_audio reference in menu.gd and adjusted the start button delay. Also added export_presets.cfg for Windows Desktop export configuration.

--- scripts/ui/menu.gd
+++ scripts/ui/menu.gd
@@ -4,6 +4,7 @@ extends PanelContainer
 @onready var credits_button = $marign/layout/buttons/credits
 @onready var quit_button = $marign/layout/buttons/quit
 @onready var title_label = $marign/layout/buttons/Game_Name
+@onready var click_audio: AudioStreamPlayer = $marign/ClickAudio
 
 var button_original_positions = {}
 
@@ -49,7 +50,7 @@ func _on_button_unhover(button: Button):
 
 func _on_start_pressed() -> void:
 	click_audio.play()
-	await get_tree().create_timer(0.1).timeout
+	await get_tree().create_timer(0.07).timeout
 	# Ensure we go to the latest level
 	var latest_level_number = int(Global.current_level)
 	var next_scene_path = "res://scenes/levels/level_" + str(latest_level_number) + ".tscn"
